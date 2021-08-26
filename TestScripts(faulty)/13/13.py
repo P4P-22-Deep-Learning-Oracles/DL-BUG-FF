b43bc12c436d10c327e49d36af7db959e8704f81
Michael O'Connor <B>
2021-08-26 16:27:33 +1200

    Adjusted TestScript
    changed 6 and deleted obsolete

--- TestScripts(faulty)/13/13.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug 13 02:17:55 2021
-
-@author: Michael
-
-Buggy code sample 13 as on excel
-"""
-loaded_state = torch.load(model_path+seq_to_seq_test_model_fname)
--- TestScripts(faulty)/5/5.py
+++ TestScripts(faulty)/5/5.py
@@ -6,22 +6,5 @@ Created on Fri Aug 13 02:17:55 2021
 
 Buggy code sample 5 as on excel
 """
-from keras.models import Sequential
-from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.model_selection import KFold
-from sklearn.model_selection import cross_val_score
-from keras.layers import Dense
-
-def neuralnetmodel():
-    #Crete model
-    model = Sequential()
-    model.add(Dense(13, input_dim = 13, kernel_initializer = 'normal', activation = 'relu'))
-
-model.add(Dense(1, kernel_initializer = 'normal', activation = 'relu'))
-model.add(Dense(1, kernel_initializer = 'normal', activation = 'relu'))
-## Output layer
-model.add(Dense(1, kernel_initializer = 'normal'))
-
-#Compile model
-model.compile(loss = 'mean_squared_error', optimizer = 'adam')
-return model
+image_string = tf.read_file(filename)
+image_decoded = tf.image.decode_jpeg(image_string, channels=3)
