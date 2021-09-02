f6facf281c89257285289f509a63102bbd434e80
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-09-02 23:29:51 +1200

    Pattern finding implemented

--- TS/3/binary_bug.py
+++ TS/3/binary_bug.py
@@ -1,9 +1,8 @@
-import keras from keras.datasets
-import cifar10 from keras.preprocessing.image
-import ImageDataGenerator from keras.models
 import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K import os
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import os
 
 
 # Constants used
@@ -18,7 +17,8 @@ epochs = 5
 
 
 if K.image_data_format() == 'channels_first':
-    input_shape = (3, img_width, img_height) else:
+    input_shape = (3, img_width, img_height)
+else:
     input_shape = (img_width, img_height, 3)
 
 model = Sequential([
@@ -58,7 +58,7 @@ model.compile(loss='binary_crossentropy',
               metrics=['accuracy'])
 
 # no augmentation config train_datagen = ImageDataGenerator() validation_datagen = ImageDataGenerator()
-     train_generator = train_datagen.flow_from_directory(
+train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
