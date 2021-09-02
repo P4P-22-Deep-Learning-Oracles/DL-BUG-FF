e5f6b1d98a816dc76390dfdbeecc1acc4e3bace9
Wilson Baker <49139851+WilsonBaker@users.noreply.github.com>
2021-09-02 21:10:01 +1200

    Bug fixes for AST test scripts

--- TS/3/3.py
+++ TS/3/3.py
@@ -6,13 +6,16 @@ Created on Fri Aug 13 02:17:55 2021
 
 Buggy code sample 3 as on excel
 """
+import tensorflow as tf
+import csv
+
 with tf.Session() as sess:
-model_saver = tf.train.import_meta_graph(model_save_folder + '/my-model.meta')
-model_saver.restore(sess, model_save_folder + '/my-model')
-x = tf.placeholder('float')
-output = tf.get_collection("output")[0] #output will be the tensor for model's last layer
-print("Model restored.")
-print('Initialized')
+    model_saver = tf.train.import_meta_graph(model_save_folder + '/my-model.meta')
+    model_saver.restore(sess, model_save_folder + '/my-model')
+    x = tf.placeholder('float')
+    output = tf.get_collection("output")[0] #output will be the tensor for model's last layer
+    print("Model restored.")
+    print('Initialized')
 #print(sess.run(tf.get_default_graph().get_tensor_by_name('w_conv1:0')))
 
 #collect list of preprocessed data on submission set
