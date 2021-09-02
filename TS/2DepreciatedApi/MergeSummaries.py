b53299b9e94e4841bae13e88a7cfc6e751647632
Michael O'Connor <B>
2021-09-02 18:13:23 +1200

    Depreicated Api fixing

---
+++ TS/2DepreciatedApi/MergeSummaries.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+
+accuracy_summary = tf.scalar_summary("accuracy", 0.345)
+loss_summary = tf.scalar_summary("loss", 0.345)
+
+merged = tf.merge_summary([accuracy_summary, loss_summary])
